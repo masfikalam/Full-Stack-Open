6f0e4a26b3715a413b23b11392868ef05d99875d
Masfikul Alam <masfikalamfrp@gmail.com>
2021-09-28 02:58:08 +0600

    phonebook crud operation done

--- part-2/phonebook/src/services/persons.js
+++ part-2/phonebook/src/services/persons.js
@@ -3,10 +3,12 @@ const baseURL = "https://phonebook-fso-2021.herokuapp.com/api/persons";
 
 const getContacts = () => axios.get(baseURL);
 
+const getOne = (id) => axios.get(`${baseURL}/${id}`);
+
 const addContact = (obj) => axios.post(baseURL, obj);
 
 const deleteContact = (id) => axios.delete(`${baseURL}/${id}`);
 
 const updateContact = (id, obj) => axios.put(`${baseURL}/${id}`, obj);
 
-export { getContacts, addContact, deleteContact, updateContact };
+export { getContacts, getOne, addContact, deleteContact, updateContact };
